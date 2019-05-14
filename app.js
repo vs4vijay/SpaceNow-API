601186715d9b0e3b3f5d673cc16f007d5d8d6e91
vs4vijay <vs4vijay@gmail.com>
2019-05-14 23:56:52 +0530

    Add validations on env config

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
+'use strict';
 
-import 'dotenv/config';
 import express from 'express';
 
 import config from './config';
-
 import routes from './src/routes';
 
+
 const app = express();
-// const PORT = process.env.SERVER_PORT || 9000;
 
 const logger = (req, res, next) => {
     console.log(`[LOG]`);
     next();
 };
 
+
+// Application Middlewares
 // app.use(logger);
 app.use(express.json());
 
+
+// Health Check API
 app.get('/', function(req, res) {
     res.send({success: true, message: 'Server is Running'});
 });
 
 
-// Add Routes
+// Application Routes
 app.use('/api/v1', routes.users);
 
 
-
+// Start Server
 app.listen(config.server.port, () => {
-    console.log(`Server Started on Port: ${config.server.port}`);
+    console.log(`[+] Server Started on Port: ${config.server.port}`);
 });
--- config/config.js
+++ config/config.js
@@ -1,23 +1,42 @@
 'use strict';
 
+import 'dotenv/config';
+import joi from '@hapi/joi';
+
+
 // Required environment variables
-[
-  'SERVER_ENV',
-  'SERVER_PORT'
-].forEach((name) => {
+const requiredEnvVariables = ['SERVER_ENV', 'SERVER_PORT'];
+
+requiredEnvVariables.forEach(name => {
   if (!process.env[name]) {
     throw new Error(`[-] Environment variable ${name} is missing`)
   }
 });
 
+
+const envVariablesSchema = joi.object().keys({
+  SERVER_ENV: joi.string().valid(['local', 'dev', 'qa', 'prod']).required(),
+  SERVER_PORT: joi.number().required(),
+  LOG_LEVEL: joi.string().allow(['error', 'warn', 'info', 'verbose', 'debug']).default('info')
+}).unknown().required();
+
+
+const { error, value: envVariables } = joi.validate(process.env, envVariablesSchema);
+
+console.log('envVariables', envVariables);
+
+if (error) {
+  throw new Error(`[-] Config validation error: ${error.message}`);
+}
+
 const config = {
-  env: process.env.SERVER_ENV,
+  env: envVariables.SERVER_ENV,
   logger: {
-    level: process.env.LOG_LEVEL || 'info'
+    level: envVariables.LOG_LEVEL
   },
   server: {
-    port: Number(process.env.SERVER_PORT)
+    port: Number(envVariables.SERVER_PORT)
   }
-}
+};
 
 export default config;
